c1426a24a28bf9a8b0919171d2e4c38653cc9466
Jurass72 <jezyna2011@gmail.com>
2026-03-24 19:07:30 +0100

    added main class

---
+++ game.py
@@ -0,0 +1,17 @@
+ETAPY = {
+    1:{"nazwa": "Cortina",
+        "dystans": 15,
+        "nachylenie": 7,
+        "trudność": "łatwy", 
+        "coords": (46.540, 12.135),
+},
+2:{"nazwa": "Marmolada",
+        "dystans": 12,
+        "nachylenie": 10,
+        "trudność": "średni", 
+        "coords": (46.45, 11.85)
+},
+}
+def nowy_stan():
+    return {"energia": 100, "pozycja": 0, "czas": 0, "etap": 1}
+
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def main():
-    print("Hello from wycieczka!")
+from game import utworz_okno, nowy_stan, ETAPY
+from ui import utworz_okno
+class Wycieczka:
+    def __init__(self):
+        self.okno = utworz_okno()
+        self.frame = None
+        self.stan = nowy_stan()
+        self.etap = 1
+
+        self.start()
 
 if __name__ == "__main__":
-    main()
+    pass
